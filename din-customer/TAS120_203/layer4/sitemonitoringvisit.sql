50f236bc4a63f54a3bcebcb6fbe063281514c678
aditya.chavan@saama.com <aditya.chavan@saama.com>
2022-03-02 12:12:11 +0530

    SiteMonitoringVisit Changes

--- din-customer/TAS120_203/layer4/sitemonitoringvisit.sql
+++ din-customer/TAS120_203/layer4/sitemonitoringvisit.sql
@@ -33,7 +33,8 @@ WITH included_sites AS (
                 SELECT  'TAS120_203'::text AS studyid,
                         concat('TAS120_203_',site_number)::text AS siteid,
                         visit_type ::text AS visitname,
-                        nullif(start_date_of_conducted_visit,'') ::date AS visitdate,
+                        --nullif(start_date_of_conducted_visit,'') ::date AS visitdate,
+                        coalesce(nullif("start_date_of_conducted_visit"::text,''),nullif("planned_visit_date"::text,'')) ::date AS visitdate,
                         null::text AS smvipyn,
                         null::text AS smvpiyn,
                        (TO_DATE(nullif(visit_end_date,''),'DD-Mon-YYYY') - TO_DATE(nullif(start_date_of_conducted_visit,''),'DD-Mon-YYYY')) ::text as smvtrvld,
@@ -63,5 +64,6 @@ SELECT
         /*KEY , now()::timestamp with time zone AS comprehend_update_time KEY*/
 FROM sitemonitoringvisit_data smv
 JOIN included_sites si ON (smv.studyid = si.studyid AND smv.siteid = si.siteid);
+
 
 
--- din-customer/TAS120_203/layer4/sitemonitoringvisitschedule.sql
+++ din-customer/TAS120_203/layer4/sitemonitoringvisitschedule.sql
@@ -10,7 +10,7 @@ WITH included_sites AS (
                SELECT distinct 'TAS120_203'::text AS studyid,
                         concat('TAS120_203_',"site_number")::text AS siteid,
 						 visit_type||'~' || row_number() OVER(partition by "visit_type","site_number" ORDER BY "start_date_of_conducted_visit" ASC)::text AS visitname,
-						 nullif("planned_visit_date",'')::date AS plannedvisitdate,
+						 coalesce(nullif("planned_visit_date"::text,''),nullif("start_date_of_conducted_visit"::text,''))::date AS plannedvisitdate,
 						 "visit_type"::text as smvvtype
 				 from tas120_203_ctms.study_visit)
 
@@ -25,10 +25,6 @@ SELECT
         /*KEY , now()::timestamp with time zone AS comprehend_update_time KEY*/
 FROM sitemonitoringvisitschedule_data smvs
 JOIN included_sites si ON (smvs.studyid = si.studyid AND smvs.siteid = si.siteid);
-
-
-
-
 
 
 
